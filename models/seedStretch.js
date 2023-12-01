13e0313fabd479bac2393c3dfb027b7dfc3c35e7
Haroon Khan <hronkkhan6@gmail.com>
2023-11-30 21:52:38 -0800

    projjject

--- models/seedStretch.js
+++ models/seedStretch.js
@@ -2,50 +2,78 @@ module.exports = [
     {
         name: "Dynamic Warmup",
         muscle: "Chest, Back",
-        time: 60,
+        time: 45,
         description: "Start in the position shown below - ",
         image: "https://www.meanmuscles.com/develop/wp-content/uploads/2018/10/dynamic-chest-stretches-600x373.png"
     },
     {
         name: "One Arm Wall Lean",
-        muscle: "Chest, Biceps, Forearms",
+        muscle: "Chest, Bicep, Forearm",
         time: 45,
         description: "Start in the position shown below - ",
         image: "https://www.meanmuscles.com/wp-content/uploads/2020/09/one-arm-wall-stretches.png"
     },
     {
         name: "Arms Behind The Back Pull",
-        muscle: "Biceps",
+        muscle: "Bicep",
         time: 30,
         description: "Start in the position shown below - ",
         image: "https://www.meanmuscles.com/wp-content/uploads/2020/09/behind-the-back-bicep-stretch.png"
     },
     {
         name: "One Arm Overhead Pull",
-        muscle: "Triceps, Back",
+        muscle: "Tricep, Back",
         time: 30,
         description: "Start in the position shown below - ",
         image: "https://www.meanmuscles.com/wp-content/uploads/2020/09/overhead-triceps-stretch.png"
     },
     {
-        name: "",
-        muscle: "",
-        time: 3,
+        name: "Cross Body Arm Pull",
+        muscle: "Tricep, Shoulder",
+        time: 30,
         description: "Start in the position shown below - ",
-        image: ""
+        image: "https://www.meanmuscles.com/wp-content/uploads/2020/09/frontal-triceps-stretch.png"
     },
     {
-        name: "",
-        muscle: "",
-        time: 3,
+        name: "Lying Dip Lean",
+        muscle: "Chest, Back, Shoulder, Tricep",
+        time: 60,
         description: "Start in the position shown below - ",
-        image: ""
+        image: "https://www.meanmuscles.com/wp-content/uploads/2020/09/triceps-dip-stretch.png"
     },
     {
-        name: "",
-        muscle: "",
-        time: 3,
+        name: "Butterfly",
+        muscle: "Hip Adducter",
+        time: 60,
         description: "Start in the position shown below - ",
-        image: ""
+        image: "https://www.meanmuscles.com/wp-content/uploads/2020/09/butterfly-stretch.png"
+    },
+    {
+        name: "Knee to Chest Leg Extension",
+        muscle: "Glute, Hamstring",
+        time: 60,
+        description: "Start in the position shown below - ",
+        image: "https://www.meanmuscles.com/wp-content/uploads/2020/09/hamstring-stretch.png"
+    },
+    {
+        name: "Heel to Back Leg Flexion",
+        muscle: "Quadricep",
+        time: 60,
+        description: "Start in the position shown below - ",
+        image: "https://www.fitadium.com/img/cms/Images%20billets/etirement-quadriceps.png"
+    },
+    {
+        name: "Leaning Wall Ankle Flexion",
+        muscle: "Calf",
+        time: 60,
+        description: "Start in the position shown below - ",
+        image: "https://www.meanmuscles.com/wp-content/uploads/2020/09/calf-stretches.png"
+    },
+    {
+        name: "Band Hip Abduction",
+        muscle: "Hip Abductor",
+        time: 60,
+        description: "Start in the position shown below - ",
+        image: "https://www.inspireusafoundation.org/wp-content/uploads/2022/01/band-hip-abduction-1024x519.png"
     },
 ]
