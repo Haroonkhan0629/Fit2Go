b4bc372585d27fd7f27c5f1d30bcf20ff8b1afc3
Haroon Khan <hronkkhan6@gmail.com>
2023-12-02 07:31:36 -0800

    final project

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const savedController = require("./controllers/saved.js")
 const PORT = process.env.PORT || 3000
 const mongodbURI = process.env.MONGODBURI
 
-mongoose.connect("mongodb://127.0.0.1:27017")
+mongoose.connect(mongodbURI + "test")
 mongoose.connection.once("open", function () {
     console.log("connected to mongo")
 })
